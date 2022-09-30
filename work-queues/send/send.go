e158e9f865fdef091d7254f35cddc97fa8cc6cf1
lostsquirrel <lostsquirreli@hotmail.com>
2022-09-30 17:34:05 +0800

    message durablility

--- work-queues/send/send.go
+++ work-queues/send/send.go
@@ -18,12 +18,12 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		"task_queue", // name
+		true,         // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
