5f5aaca260415c3deed01a1513f9d8647989d57c
lostsquirrel <lostsquirreli@hotmail.com>
2022-09-30 17:44:10 +0800

    fair dispatch

--- work-queues/receive/receive.go
+++ work-queues/receive/receive.go
@@ -32,11 +32,16 @@ func main() {
 		nil,          // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
-
+	err = ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	failOnError(err, "Failed to set QoS")
 	msgs, err := ch.Consume(
 		q.Name, // queue
 		"",     // consumer
-		true,   // auto-ack
+		false,  // auto-ack
 		false,  // exclusive
 		false,  // no-local
 		false,  // no-wait
---
+++ work-queues/send/readme.md
@@ -0,0 +1,7 @@
+
+go run send.go 1.
+go run send.go 2..
+go run send.go 3.
+go run send.go 4....
+go run send.go 5.
+go run send.go 6......
